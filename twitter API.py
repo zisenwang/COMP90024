de39a448b8d848ca6cc7384769e1cb266f49ee55
boyang1123 <byyan1@student.unimelb.edu.au>
2022-04-15 20:21:18 +0800

    some sample when use tweepy to get tweets

---
+++ twitter API.py
@@ -0,0 +1,27 @@
+import tweepy
+import configparser
+import pandas as pd
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#public_tweets = api.home_timeline()
+search_words = "#covid-19,#melbourne"
+data_since = "2021-1-1"
+tweets =tweepy.Cursor(api.search_tweets, q=search_words,lang = "en",since=data_since).items(10)
+columns = ['Time', 'User', 'Tweet']
+data = []
+for tweet in tweets:
+    data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
+df = pd.DataFrame(data, columns=columns)
+
+df.to_csv('tweets1.csv')
