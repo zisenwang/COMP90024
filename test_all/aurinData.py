debac4a2d029687287ee44d69b718e3e9db303ad
wangzs <wangzs@si-tech.com.cn>
2022-05-03 05:24:29 +0800

    word cloud, dynamic map-reduce, new version of couchdb, part of aurin..

---
+++ test_all/aurinData.py
@@ -0,0 +1,21 @@
+import json
+#
+# output:  area_name:{age_group:{gender,aided,unaided}...}...
+def aurinDisabledData():
+    f = open('./inner.json',encoding='utf8')
+    j = json.load(f)
+    collection = {}
+    for i in range(len(j['features'])):
+        raw = j['features'][i]['properties']
+        for key in raw:
+            # handle each attribute
+            keyNameList = key.split('_')
+
+            # the attribute indicates the area name
+            if (keyNameList[1][:4] == 'name'):
+                collection[raw[key]] = {}
+
+            
+    print(collection)
+            
+    f.close()
